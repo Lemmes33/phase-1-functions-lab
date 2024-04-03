397b9e7d1a1486ffa87cac5123cae7b1116800df
lemmes33 <vinlemuel021@gmail.com>
2024-04-03 14:51:33 +0300

    Calculating distance

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+// question 1
+function distanceFromHqInBlocks(pickupLocation){
+    return Math.abs(pickupLocation -42);
+}
+// question 2
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks (pickupLocation)*264
+}
+// question 3
+function distanceTravelledInFeet(startBlock, endBlock) {
+    const distanceInBlocks = Math.abs(endBlock - startBlock);
+    const distanceInFeet = distanceInBlocks * 264;
+    return distanceInFeet;
+  }
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distanceInFeet = distanceTravelledInFeet(startBlock, endBlock);
+  
+    // Define fare based on distance traveled
+    let farePrice;
+    if (distanceInFeet <= 400) {
+      farePrice = 0; 
+    } else if (distanceInFeet <= 2000) {
+      const distanceOverFree = distanceInFeet - 400;
+      farePrice = distanceOverFree * 0.02; 
+    } else if (distanceInFeet <= 2500) {
+      farePrice = 25;
+    } else {
+      farePrice = 'cannot travel that far'; 
+    }
+  
+    return farePrice;
+  }
+  
